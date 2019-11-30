1065e935669129c5d14f6085cb3305cc7f5d0026
Telroshan <vincent.thebaud@ynov.com>
2019-11-30 18:49:00 +0100

    Toggle

---
+++ Editor/FastEnterPlayModeToggle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+
+namespace FastEnterPlayModeToggle.Editor
+{
+    public static class FastEnterPlayModeToggle
+    {
+        [MenuItem("Fast Enter Play Mode/Enable")]
+        public static void Enable()
+        {
+            EditorSettings.enterPlayModeOptionsEnabled = true;
+        }
+        [MenuItem("Enabled", true, menuItem = "Fast Enter Play Mode/Enable")]
+        public static bool EnableValidation()
+        {
+            return !EditorSettings.enterPlayModeOptionsEnabled;
+        }
+        
+        [MenuItem("Fast Enter Play Mode/Disable")]
+        public static void Disable()
+        {
+            EditorSettings.enterPlayModeOptionsEnabled = false;
+        }
+        [MenuItem("Disabled", true, menuItem = "Fast Enter Play Mode/Disable")]
+        public static bool DisableValidation()
+        {
+            return EditorSettings.enterPlayModeOptionsEnabled;
+        }
+    }
+}
