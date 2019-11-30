e00d776dfd1d4e54bd948feafce763adb381ff44
Telroshan <vincent.thebaud@ynov.com>
2019-11-30 19:35:02 +0100

    Menu items for domain / scene reload as well

--- Editor/FastEnterPlayModeToggle.cs
+++ Editor/FastEnterPlayModeToggle.cs
@@ -1,29 +1,86 @@
 ﻿using UnityEditor;
+using UnityEngine;
 
 namespace FastEnterPlayModeToggle.Editor
 {
     public static class FastEnterPlayModeToggle
     {
-        [MenuItem("Fast Enter Play Mode/Enable")]
+        [MenuItem("Fast Enter Play Mode/Enable", priority = 0)]
         public static void Enable()
         {
             EditorSettings.enterPlayModeOptionsEnabled = true;
         }
-        [MenuItem("Enabled", true, menuItem = "Fast Enter Play Mode/Enable")]
+
+        [MenuItem("", true, menuItem = "Fast Enter Play Mode/Enable")]
         public static bool EnableValidation()
         {
             return !EditorSettings.enterPlayModeOptionsEnabled;
         }
-        
-        [MenuItem("Fast Enter Play Mode/Disable")]
+
+        [MenuItem("Fast Enter Play Mode/Disable", priority = 1)]
         public static void Disable()
         {
             EditorSettings.enterPlayModeOptionsEnabled = false;
         }
-        [MenuItem("Disabled", true, menuItem = "Fast Enter Play Mode/Disable")]
+
+        [MenuItem("", true, menuItem = "Fast Enter Play Mode/Disable")]
         public static bool DisableValidation()
         {
             return EditorSettings.enterPlayModeOptionsEnabled;
+        }
+
+        
+        [MenuItem("Fast Enter Play Mode/Enable domain reload", priority = 20)]
+        public static void EnableDomainReload()
+        {
+            EditorSettings.enterPlayModeOptions &= ~EnterPlayModeOptions.DisableDomainReload;
+        }
+
+        [MenuItem("", true, menuItem = "Fast Enter Play Mode/Enable domain reload")]
+        public static bool EnableDomainReloadValidation()
+        {
+            return EditorSettings.enterPlayModeOptionsEnabled &&
+                   (EditorSettings.enterPlayModeOptions & EnterPlayModeOptions.DisableDomainReload) ==
+                   EnterPlayModeOptions.DisableDomainReload;
+        }
+        [MenuItem("Fast Enter Play Mode/Disable domain reload", priority = 21)]
+        public static void DisableDomainReload()
+        {
+            EditorSettings.enterPlayModeOptions |= EnterPlayModeOptions.DisableDomainReload;
+        }
+        [MenuItem("", true, menuItem = "Fast Enter Play Mode/Disable domain reload")]
+        public static bool DisableDomainReloadValidation()
+        {
+            return EditorSettings.enterPlayModeOptionsEnabled &&
+                   (EditorSettings.enterPlayModeOptions & EnterPlayModeOptions.DisableDomainReload) !=
+                   EnterPlayModeOptions.DisableDomainReload;
+        }
+        
+
+        [MenuItem("Fast Enter Play Mode/Enable scene reload", priority = 40)]
+        public static void EnableSceneReload()
+        {
+            EditorSettings.enterPlayModeOptions &= ~EnterPlayModeOptions.DisableSceneReload;
+        }
+
+        [MenuItem("", true, menuItem = "Fast Enter Play Mode/Enable scene reload")]
+        public static bool EnableSceneReloadValidation()
+        {
+            return EditorSettings.enterPlayModeOptionsEnabled &&
+                   (EditorSettings.enterPlayModeOptions & EnterPlayModeOptions.DisableSceneReload) ==
+                   EnterPlayModeOptions.DisableSceneReload;
+        }
+        [MenuItem("Fast Enter Play Mode/Disable scene reload", priority = 41)]
+        public static void DisableSceneReload()
+        {
+            EditorSettings.enterPlayModeOptions |= EnterPlayModeOptions.DisableSceneReload;
+        }
+        [MenuItem("", true, menuItem = "Fast Enter Play Mode/Disable scene reload")]
+        public static bool DisableSceneReloadValidation()
+        {
+            return EditorSettings.enterPlayModeOptionsEnabled &&
+                   (EditorSettings.enterPlayModeOptions & EnterPlayModeOptions.DisableSceneReload) !=
+                   EnterPlayModeOptions.DisableSceneReload;
         }
     }
 }
